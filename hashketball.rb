5f6b956142ef88c61aba690b69e192222a86102e
Trevor Low <tlow94@uw.edu>
2020-04-12 20:07:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 def game_hash
   {
-    home: {team_name: => "", 
-      colors: => [], 
-      players: => [{player_name: => }]}, 
+    home: {team_name: => "",
+      colors: => [],
+      players: => [{player_name: => }]},
     away: {team_name: => "", colors: => [], players: => [{}]}
   }
 end
